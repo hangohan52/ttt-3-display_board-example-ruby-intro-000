6544b4d8ffb991734f15d5c64832209b0700b53f
Kenneth Han <hangohan52@gmail.com>
2020-07-01 20:05:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
  end
